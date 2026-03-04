1ea0bd21e72d56572aeb92b2a2bfa0ffe0fc0805
ka4oks1 <167628059+ka4oks1@users.noreply.github.com>
2026-03-05 00:13:53 +0300

    first commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,29 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	var err error
+	if len(dataset) == 0 {
+		return
+	}
+	for _, v := range dataset {
+
+		err = dp.Parse(v)
+
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	actInfo, err := dp.ActionInfo()
+
+	fmt.Println(actInfo)
 }
--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,65 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	parsedInfo := strings.Split(datastring, ",")
+	if len(parsedInfo) != 2 {
+
+		return errors.New("wrong length")
+
+	}
+	steps, err := strconv.Atoi(parsedInfo[0])
+	if err != nil {
+		return err
+	}
+	if steps <= 0 {
+		return errors.New("incorrect steps count")
+	}
+	ds.Steps = steps
+
+	timeDur, err := time.ParseDuration(parsedInfo[1])
+	if err != nil {
+		return err
+	}
+	if timeDur <= 0 {
+		return errors.New("incorrect duration time")
+	}
+
+	ds.Duration = timeDur
+
+	return nil
+
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+
+	dist := spentenergy.Distance(ds.Steps, ds.Height)
+
+	var spentCal float64
+	var err error
+
+	spentCal, err = spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return "", err
+	}
+
+	resultStr := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		ds.Steps, dist, spentCal)
+	return resultStr, err
 }
